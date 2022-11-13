b7cfacb29d288f3dcb4051351fad85945b38ee00
Hortigas <xxvsvitor98xx@gmail.com>
2022-11-13 18:36:38 -0300

    estados

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 import bodyParser from 'body-parser';
 import express from 'express';
 
@@ -8,27 +8,49 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.route('/:estado/cidade')
+app.route('/:estado/cidades')
     .get(async (req, res) => {
         const estado = req.params.estado;
-        const cidades = await prisma.cidade.findMany({ where: { estado: { nome: estado } } });
+        const rawCidades = await prisma.cidade.findMany({ where: { estado: { nome: estado } } });
+        const cidades = rawCidades.map((city => (city.nome)));
         res.json(cidades);
     })
     .post(async (req, res) => {
         try {
             const estadoName = req.params.estado;
             const estado = await prisma.estado.findUnique({ where: { nome: estadoName } });
-            if (!estado) throw new Error('estado não encontrado');
+            if (estado === null) throw new Error('estado não encontrado');
             const { nome } = req.body as { nome: string; };
+            if (!nome || nome.length === 0) throw new Error('formato do corpo da requisição incorreto');
             await prisma.cidade.create({ data: { nome: nome, estadoId: estado.id } });
-            res.json({ status: 'ok' });
+            res.status(200).send('ok');
         } catch (err: any) {
-            res.json({ error: err.message });
+            if (err instanceof Prisma.PrismaClientKnownRequestError) {
+                if (err.code === 'P2002')
+                    res.status(404).send('cidade já existe');
+            }
+            res.status(404).send(err.message);
         }
     })
     .put(async (req, res) => {
 
     });
+
+app.get('/estados', async (req, res) => {
+    const rawEstados = await prisma.estado.findMany();
+    const estados = rawEstados.map((estado) => estado.nome);
+    res.json(estados);
+});
+
+app.post('/estados', async (req, res) => {
+    try {
+        const estado = req.body.estado;
+        await prisma.estado.create({ data: { nome: estado } });
+        res.status(201).send();
+    } catch (err) {
+        res.status(400).send('Estado já existe');
+    }
+});
 
 // app.post('/post', async (req, res) => {
 //     const { title, content, authorEmail } = req.body;
