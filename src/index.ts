47588e45092d3701f6c35dd58efa0a4d70393c55
Felps <117868066+FelpsOL9@users.noreply.github.com>
2022-11-21 22:46:29 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -61,36 +61,36 @@ app.delete('/estados/:id', async (req, res) => {
     }
 });
 
-// app.post('/post', async (req, res) => {
-//     const { title, content, authorEmail } = req.body;
-//     const post = await prisma.post.create({
-//         data: {
-//             title,
-//             content,
-//             published: false,
-//             author: { connect: { email: authorEmail } },
-//         },
-//     });
-//     res.json(post);
-// });
+ app.post('/post', async (req, res) => {
+     const { title, content, authorEmail } = req.body;
+     const post = await prisma.post.create({
+         data: {
+             title,
+             content,
+             published: false,
+            author: { connect: { email: authorEmail } },
+         },
+     });
+     res.json(post);
+ });
 
-// app.put('/publish/:id', async (req, res) => {
-//     const { id } = req.params;
-//     const post = await prisma.post.update({
-//         where: { id },
-//         data: { published: true },
-//     });
-//     res.json(post);
-// });
+ app.put('/publish/:id', async (req, res) => {
+     const { id } = req.params;
+     const post = await prisma.post.update({
+         where: { id },
+         data: { published: true },
+     });
+     res.json(post);
+ });
 
-// app.delete('/user/:id', async (req, res) => {
-//     const { id } = req.params;
-//     const user = await prisma.user.delete({
-//         where: {
-//             id,
-//         },
-//     });
-//     res.json(user);
-// });
+ app.delete('/user/:id', async (req, res) => {
+     const { id } = req.params;
+     const user = await prisma.user.delete({
+         where: {
+id,
+         },
+    });
+     res.json(user);
+ });
 
 app.listen(3333);
